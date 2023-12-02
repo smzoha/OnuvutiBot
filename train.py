d623f2326e36652b8e5854bca14a131d832b0858
Shamah M Zoha <shamah.zoha@gmail.com>
2023-12-02 23:46:33 +0600

    Added method for tokenizing words from dataset

--- train.py
+++ train.py
@@ -1,6 +1,21 @@
 import numpy as np
 import pandas as pd
+from bnlp.tokenizer.basic import BasicTokenizer
 from sklearn.model_selection import train_test_split
+
+
+def get_tokens(X, y):
+    tokens = []
+    tokenizer = BasicTokenizer()
+
+    for sentence in X:
+        tokens.extend(tokenizer.tokenize(sentence))
+
+    for sentence in y:
+        tokens.extend(tokenizer.tokenize(sentence))
+
+    return list(set(tokens))
+
 
 data = pd.read_csv('./data/clean-data.csv')
 
@@ -9,3 +24,6 @@ y = np.array(data['Answers']).astype('str')
 
 print('Splitting dataset to train/test sets with ratio of 70-30')
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=False)
+
+tokens = get_tokens(X, y)
+print(tokens)
