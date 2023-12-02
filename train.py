2c7a61c1e4ea973b894aa8a26e44e5cf34790664
Shamah M Zoha <shamah.zoha@gmail.com>
2023-12-03 00:08:41 +0600

    Added function for obtaining GLoVe word embedding from tokens

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from bnlp.embedding.word2vec import BengaliWord2Vec
 from bnlp.tokenizer.basic import BasicTokenizer
 from sklearn.model_selection import train_test_split
 
@@ -17,7 +18,24 @@ def get_tokens(X, y):
     return list(set(tokens))
 
 
-data = pd.read_csv('./data/clean-data.csv')
+def word_embeddings(tokens, verbose):
+    glove = BengaliWord2Vec()
+    vectors = []
+    failed_tokens = []
+
+    for token in tokens:
+        try:
+            vectors.append(glove.get_word_vector(token))
+        except KeyError:
+            failed_tokens.append(token)
+
+            if verbose:
+                print('Failed to generate for token:', token)
+
+    return vectors, failed_tokens
+
+
+data = pd.read_csv('./data/clean-data.csv', encoding='utf-8-sig')
 
 X = np.array(data['Questions']).astype('str')
 y = np.array(data['Answers']).astype('str')
@@ -26,4 +44,8 @@ print('Splitting dataset to train/test sets with ratio of 70-30')
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=False)
 
 tokens = get_tokens(X, y)
-print(tokens)
+print('Generated tokens. Vocabulary count:', len(tokens))
+
+vectors, failed_tokens = word_embeddings(tokens, verbose=False)
+print('Generated Word Embeddings from Tokens. Vector Count:', len(vectors))
+print('Failed Token Count:', len(failed_tokens))
